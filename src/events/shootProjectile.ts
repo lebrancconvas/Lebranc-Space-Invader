dc5f9f245bbf6b56986b45075d44fe831a2e5b69
lebrancconvas <lebrancconvas@gmail.com>
2023-09-06 12:00:04 +0700

    🐛 fix: fix not shoot on enemy but enemy still disappeared

--- src/events/shootProjectile.ts
+++ src/events/shootProjectile.ts
@@ -3,7 +3,7 @@ import { checkCollision } from "./checkCollision";
 
 export function shootProjectile(enemy: Enemy) {
   projectiles.forEach((projectile: Projectile, index: number) => {
-    if (projectile.y <= projectile.radius || checkCollision(enemy, projectile)) {
+    if (checkCollision(enemy, projectile)) {
       projectiles.splice(index, 1);
       enemy.destroy();
     } else {
